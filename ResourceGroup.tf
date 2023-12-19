8d309e7333b51a06eed119b7d38ffae875a5812c
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2023-12-18 20:34:53 -0500

    Update ResourceGroup.tf

--- ResourceGroup.tf
+++ ResourceGroup.tf
@@ -8,6 +8,7 @@ resource "azurerm_storage_account" "mcitstorage" {
   location                 = azurerm_resource_group.mcit420zz5um.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+  cross_tenant_replication_enabled = false
 
   tags = {
     environment = "staging"
