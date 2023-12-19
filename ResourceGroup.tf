d77befe0fbd106397ca879a0a7346820880e1f11
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2023-12-18 20:30:38 -0500

    Update ResourceGroup.tf

--- ResourceGroup.tf
+++ ResourceGroup.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "mcit420zz5um" {
   name     = "mcit_resource_group_420-ZZ5-UM"
   location = "canadacentral"
 }
+resource "azurerm_storage_account" "mcitstorage" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.mcit420zz5um.name
+  location                 = azurerm_resource_group.mcit420zz5um.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
