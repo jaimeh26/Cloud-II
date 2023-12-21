3a215208088121e548e18cd9c6c6444479966fc6
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2023-12-20 20:13:23 -0500

    Update local.tf

--- local.tf
+++ local.tf
@@ -1,7 +1,7 @@
-locals (
+locals {
 service_name = "cloud2"
 owner        = "mcit-student"
 location     = "montreal"
 lenghtofservice_name=length(local.service_name)
 lengthofowner=length(local.owner)
-)
+}
