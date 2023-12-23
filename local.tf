a0e01ec144811ab053b97467119f83cb5059fd52
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2023-12-22 20:03:18 -0500

    Update local.tf

--- local.tf
+++ local.tf
@@ -7,4 +7,5 @@ lengthofowner=length(local.owner)
 getthelength=length(local.location)
 wintersports=["icehockey","snowboarding","iceskating"]
 canadianfoodlist=["poutine","smokemeat","patechinois","macaroni","salmon","pizza","hotdog","steak","lasagna"]
+numbersfromonetoten=["1","2","3","4","5","6","7","8","9","10"]
 }
