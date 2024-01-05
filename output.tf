a92c953def4b9d0279e992efba78b7b131faee89
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-05 17:08:05 -0500

    Update output.tf

--- output.tf
+++ output.tf
@@ -40,7 +40,4 @@ output "sumoftennumbers2"{
 output "storage_account_names" {
   value = [for storage_account in azurerm_storage_account.mcit : storage_account.name]
 }
-output "users"{
-      value=var.users
-}
 
