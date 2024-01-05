59ae4596001a848d116f405542922b09fd23f771
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-04 19:04:06 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,1 +1,12 @@
+resource "azurerm_storage_account" "mcit" {
+  name                     = "${var.myname}mcitstorage${var.countNumber}"
+  resource_group_name      = azurerm_resource_group.mcit420zz5um.name
+  location                 = azurerm_resource_group.mcit420zz5um.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  cross_tenant_replication_enabled = false
 
+  tags = {
+    environment = "staging"
+  }
+}
