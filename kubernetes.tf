198ff3bc813d082b9e22ed08da384e86922b1ad2
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-07 18:23:54 -0500

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -2,7 +2,7 @@ locals{
   cluster_names=["kbtjaime1","kbtjaime2","kbtjaime3","kbtjaime4","kbtjaime5"]
 }
 resource "azurerm_resource_group" "mcit420zz5um" {
-  name     = "MCIT_resource_group"
+  name     = "mcit_resource_group_420-ZZ5-UM"
   location = "Canada Central"
 }
 resource "azurerm_kubernetes_cluster" "batchabcd" {
