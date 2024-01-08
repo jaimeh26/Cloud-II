55b44760e2ef72585c38f47157b6a66c9101e716
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-08 18:44:34 -0500

    Create map.tf

---
+++ map.tf
@@ -0,0 +1,28 @@
+variable "example_map"{
+  type=map(object({
+  name=string
+  enemies_destroyed = number
+  badguy = bool
+}))
+default = {
+  key1 = {
+  name = "luke"
+  enemies_destroyed = 4552
+  badguy = false
+}
+key2 = {
+  name = "yoda"
+  enemies_destroyed = 900
+  badguy = false
+}
+key3 = {
+  name = "darth"
+  enemies_destroyed = 20000056894
+  badguy = true
+}
+
+
+
+
+
+
