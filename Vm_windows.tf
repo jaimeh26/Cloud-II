5d4216f897e10cb413334d9f170afe82c23a9b19
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-07 23:20:54 -0500

    Update Vm_windows.tf

--- Vm_windows.tf
+++ Vm_windows.tf
@@ -8,9 +8,9 @@ resource "azurerm_service_plan" "mcitsp" {
 locals{ 
   subnet_ids=["subnet1","subnet2"]
 }
-resource "azurerm_linux_web_app" "mcit_linux" {
+resource "azurerm_windows_web_app" "mcit_windows" {
   for_each            = {for subnet_id in local.subnet_ids: subnet_id=>subnet_id}
-  name                = "${var.prefix}linux-web-app"
+  name                = "${var.prefix}windows-web-app"
   resource_group_name = azurerm_resource_group.mcit420zz5um.name
   location            = azurerm_resource_group.mcit420zz5um.location
   service_plan_id     = azurerm_service_plan.mcitsp.id
