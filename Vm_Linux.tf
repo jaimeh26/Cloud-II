f9297f538546461006c93c7f0e6acb4afe5d54d0
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-07 22:28:09 -0500

    Update Vm_Linux.tf

--- Vm_Linux.tf
+++ Vm_Linux.tf
@@ -11,7 +11,7 @@ resource "azurerm_linux_web_app" "mcit_linux" {
   resource_group_name = azurerm_resource_group.mcit420zz5um.name
   location            = azurerm_resource_group.mcit420zz5um.location
   service_plan_id     = azurerm_service_plan.mcitsp.id
-  virtual_network_subnet_id = azure_virtual_network.mcitvn["subnet2"].id
+  virtual_network_subnet_id = azurerm_virtual_network.mcitvn["subnet2"].id
 
   site_config {}
 }
