ad2354f0d9f4d0ccb248374b730ab4d23afb1e87
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-07 21:29:22 -0500

    Update Vm_Linux.tf

--- Vm_Linux.tf
+++ Vm_Linux.tf
@@ -7,7 +7,7 @@ resource "azurerm_service_plan" "mcitsp" {
 }
 
 resource "azurerm_linux_web_app" "mcit_linux" {
-  name                = "example"
+  name                = "${var.prefix}linux_web_app"
   resource_group_name = azurerm_resource_group.mcit420zz5um.name
   location            = azurerm_service_plan.mcit420zz5um.location
   service_plan_id     = azurerm_service_plan.mcitsp.id
