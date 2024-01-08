e599750ad83887dd693968a96a0f435ba22be7d6
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-07 23:18:53 -0500

    Update Vm_windows.tf

--- Vm_windows.tf
+++ Vm_windows.tf
@@ -1,1 +1,20 @@
+resource "azurerm_service_plan" "mcitsp" {
+  name                = "${var.prefix}service_plan"
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+  location            = azurerm_resource_group.mcit420zz5um.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+locals{ 
+  subnet_ids=["subnet1","subnet2"]
+}
+resource "azurerm_linux_web_app" "mcit_linux" {
+  for_each            = {for subnet_id in local.subnet_ids: subnet_id=>subnet_id}
+  name                = "${var.prefix}linux-web-app"
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+  location            = azurerm_resource_group.mcit420zz5um.location
+  service_plan_id     = azurerm_service_plan.mcitsp.id
+  
 
+  site_config {}
+}
