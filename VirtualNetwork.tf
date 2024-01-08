5330cb8e4054bf617379a985410bc807913c4350
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-07 19:03:28 -0500

    Update VirtualNetwork.tf

--- VirtualNetwork.tf
+++ VirtualNetwork.tf
@@ -3,7 +3,7 @@ resource "azurerm_network_security_group" "mcitsg" {
   location            = azurerm_resource_group.mcit420zz5um.location
   resource_group_name = azurerm_resource_group.mcit420zz5um.name
 }
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "mcitvn" {
   name                = "example-network"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
