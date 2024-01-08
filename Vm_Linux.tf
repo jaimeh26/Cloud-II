0de4b4c3ebe5e6363ea285066c5a7c07eb30c0bc
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-07 22:37:18 -0500

    Update Vm_Linux.tf

--- Vm_Linux.tf
+++ Vm_Linux.tf
@@ -1,17 +1,1 @@
-resource "azurerm_service_plan" "mcitsp" {
-  name                = "${var.prefix}service_plan"
-  resource_group_name = azurerm_resource_group.mcit420zz5um.name
-  location            = azurerm_resource_group.mcit420zz5um.location
-  os_type             = "Linux"
-  sku_name            = "P1v2"
-}
 
-resource "azurerm_linux_web_app" "mcit_linux" {
-  name                = "${var.prefix}linux-web-app"
-  resource_group_name = azurerm_resource_group.mcit420zz5um.name
-  location            = azurerm_resource_group.mcit420zz5um.location
-  service_plan_id     = azurerm_service_plan.mcitsp.id
-  virtual_network_subnet_id = azurerm_virtual_network.mcitvn[each.key].subnet["subnet2"].id
-
-  site_config {}
-}
