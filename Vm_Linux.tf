26a9b0a002c6ad6f3170d07505d83db60a7f2ffc
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-07 23:00:17 -0500

    Update Vm_Linux.tf

--- Vm_Linux.tf
+++ Vm_Linux.tf
@@ -5,18 +5,13 @@ resource "azurerm_service_plan" "mcitsp" {
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
-locals {
-  subnet_ids = {
-    for key, vnet in azurerm_virtual_network.mcitvn : key => vnet.subnet["subnet2"].id
-  }
-}
 
 resource "azurerm_linux_web_app" "mcit_linux" {
-  name                = "${var.prefix}linux-web-app-${keys(azurerm_virtual_network.mcitvn)[count.index]}"
+  name                = "${var.prefix}linux-web-app"
   resource_group_name = azurerm_resource_group.mcit420zz5um.name
   location            = azurerm_resource_group.mcit420zz5um.location
   service_plan_id     = azurerm_service_plan.mcitsp.id
-  virtual_network_subnet_id = local.subnet_ids[keys(azurerm_virtual_network.mcitvn)[count.index]]
+  virtual_network_subnet_id = azurerm_virtual_network.mcitvn[each.key].subnet["subnet2"].id
 
   site_config {}
 }
