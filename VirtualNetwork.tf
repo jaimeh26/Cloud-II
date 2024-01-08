05bafa94e7fa8e70029405486aa20da7f8e3dcbe
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-07 20:59:13 -0500

    Update VirtualNetwork.tf

--- VirtualNetwork.tf
+++ VirtualNetwork.tf
@@ -8,8 +8,8 @@ locals{
 }
 resource "azurerm_virtual_network" "mcitvn" {
   name                = "example-network"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.mcit420zz5um.location
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
