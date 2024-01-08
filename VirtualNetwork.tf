df05244674382d5194a57aff4356c701a5306387
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-07 21:00:33 -0500

    Update VirtualNetwork.tf

--- VirtualNetwork.tf
+++ VirtualNetwork.tf
@@ -7,6 +7,7 @@ locals{
   vnet_names=["vnet-jaime1","vnet-jaime2","vnet-jaime3","vnet-jaime4"]
 }
 resource "azurerm_virtual_network" "mcitvn" {
+  for_each            = {for vnet in local.vnet_names: vnet=>vnet}
   name                = "example-network"
   location            = azurerm_resource_group.mcit420zz5um.location
   resource_group_name = azurerm_resource_group.mcit420zz5um.name
