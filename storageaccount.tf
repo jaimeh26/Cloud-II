00595f48b11f1249229a3785f277004748a77ef0
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-08 20:36:32 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "mcit" {
   count                    = var.storage_account_count
-  name                     = "${var.first_name}mcit${var.last_name}${count.index + 1}"
+  name                     = "${var.first_name}mcit${var.last_name}${count.index}"
   resource_group_name      = azurerm_resource_group.mcit420zz5um.name
   location                 = azurerm_resource_group.mcit420zz5um.location
   account_tier             = "Standard"
