c59e49a7c7c180522ca21adf222e3954aa1a3770
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-08 20:38:17 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "mcit" {
-  count                    = var.storage_account_count
+  count                    = 5
   name                     = "${var.first_name}mcit${var.last_name}${count.index}"
   resource_group_name      = azurerm_resource_group.mcit420zz5um.name
   location                 = azurerm_resource_group.mcit420zz5um.location
