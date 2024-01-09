e5bac84e269c1e6485bbb76dd7efe6435f3414d4
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-08 20:51:03 -0500

    Update Vm_Linux.tf

--- Vm_Linux.tf
+++ Vm_Linux.tf
@@ -10,7 +10,7 @@ locals{
 }
 resource "azurerm_linux_web_app" "mcit_linux" {
   count               = 5
-  for_each            = {for subnet_id in local.subnet_ids: subnet_id=>subnet_id}
+  #for_each            = {for subnet_id in local.subnet_ids: subnet_id=>subnet_id}
   name                = "${var.prefix}linux-web-app"${count.index}
   resource_group_name = azurerm_resource_group.mcit420zz5um.name
   location            = azurerm_resource_group.mcit420zz5um.location
