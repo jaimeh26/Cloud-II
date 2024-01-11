f9de61d47332be42af47a7de4106e9d830bf6b2b
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-10 19:23:49 -0500

    Update Variable.tf

--- Variable.tf
+++ Variable.tf
@@ -82,7 +82,7 @@ variable "simple_nested_list" {
 }
 variable "nested_map" {
   type    = map(map(string))
-  default = {
+  default = {}
     group1 = {"name" = "Alice", "age" = "25"}
     group2 = {"name" = "Bob", "age" = "30"}
   }
