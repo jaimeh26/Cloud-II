abc32221e3e0746bfbe53bd024bcf0b6e972152d
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-14 17:59:19 -0500

    Update readyaml_listofstorageaccount.tf

--- readyaml_listofstorageaccount.tf
+++ readyaml_listofstorageaccount.tf
@@ -2,12 +2,12 @@ locals{
   storage_account=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
   storage_account_list = flatten([
     for storage_account in local.storage_account : [
-      for storageaccounts in try(account.listofstorageaccount, []) :{
-        name="listofstorageaccounts.name"
-        account_tier="listofstorageaccounts.account_tier"
-        account_replication_type="listofstorageaccounts.account_replication_type"
-        access _tier="listofstorageaccounts.access_tier"
-        cross_tenant_replication_enabled="listofstorageaccounts.cross_tenant_replication_enabled"
+      for storageaccounts in try(storageaccount.listofstorageaccount, []) :{
+        name=listofstorageaccounts.name
+        account_tier=listofstorageaccounts.account_tier
+        account_replication_type=listofstorageaccounts.account_replication_type
+        access _tier=listofstorageaccounts.access_tier
+        cross_tenant_replication_enabled=listofstorageaccounts.cross_tenant_replication_enabled
         
       }
     ]
