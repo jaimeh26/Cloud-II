63e688bcc1d8eb544f8256673336d2c2a5634f06
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-14 18:45:25 -0500

    Update readyaml_storageaccount.tf

--- readyaml_storageaccount.tf
+++ readyaml_storageaccount.tf
@@ -2,7 +2,7 @@ locals{
   storage_account = [for f in fileset("${path.module}/storageaccountconfig", "[^_]*.yaml") : yamldecode(file("${path.module}/storageaccountconfig/${f}"))]
   storage_account_list = flatten([
     for storage_account in local.storage_account : [
-      for storageaccounts in try(storageaccount.listofstorageaccount, []) :{
+      for storageaccounts in try(storage.listofstorageaccount, []) :{
         name = listofstorageaccounts.name
         account_tier = listofstorageaccounts.account_tier
         account_replication_type = listofstorageaccounts.account_replication_type
