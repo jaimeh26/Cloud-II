26f112b563bb02ca778072d63c11d3bfb2ad04dd
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-14 17:35:37 -0500

    Update readyaml_listofstorageaccount.tf

--- readyaml_listofstorageaccount.tf
+++ readyaml_listofstorageaccount.tf
@@ -1,1 +1,12 @@
-
+locals{
+  storage_account=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
+  storage_account_list = flatten([
+    for app in local.linux_app : [
+      for linuxapps in try(app.listoflinuxapp, []) :{
+        name=linuxapps.name
+        os_type=linuxapps.os_type
+        sku_name=linuxapps.sku_name     
+      }
+    ]
+])
+}
