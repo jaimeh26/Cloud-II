7c23c091d62c2267f8b152c91de492bc085bebe9
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-14 21:24:15 -0500

    Update readyaml_linuxapp.tf

--- readyaml_linuxapp.tf
+++ readyaml_linuxapp.tf
@@ -13,8 +13,8 @@ locals{
 resource "azurerm_service_plan" "mcitsp" {
   for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+  location            = azurerm_resource_group.mcit420zz5um.location
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
 }
@@ -22,8 +22,8 @@ resource "azurerm_service_plan" "mcitsp" {
 resource "azurerm_linux_web_app" "batcha06webapp" {
   for_each            = azurerm_service_plan.batcha06sp
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+  location            = azurerm_resource_group.mcit420zz5um.location
   service_plan_id     = each.value.id
 
   site_config {}
