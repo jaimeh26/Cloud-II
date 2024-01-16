d59d9f6bb6aaadc9baac8ac39056699704d21a11
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-15 19:54:26 -0500

    Update readyaml_lb.tf

--- readyaml_lb.tf
+++ readyaml_lb.tf
@@ -2,13 +2,22 @@ resource "azurerm_resource_group" "example" {
   name     = "LoadBalancerRG"
   location = "West Europe"
 }
+locals{
+  waf_policy=[for f in fileset("${path.module}/waffolder", "[^_]*.yaml") : yamldecode(file("${path.module}/waffolder/${f}"))]
+  azurewafpolicy_list = flatten([
+    for app in local.waf_policy: [
+      for azurewaf in try(app.listofwafpolicy, []) :{
+        name=azurewaf.policyname
+      }
+    ]
+])
+}
 
 resource "azurerm_public_ip" "example" {
   name                = "PublicIPForLB"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"
-}
+  }
 
 resource "azurerm_lb" "example" {
   name                = "TestLoadBalancer"
