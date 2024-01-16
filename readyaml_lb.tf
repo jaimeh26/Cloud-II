1e43fd6e614864d83d76a14e2d24de2a12dbfb8d
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-15 20:27:01 -0500

    Update readyaml_lb.tf

--- readyaml_lb.tf
+++ readyaml_lb.tf
@@ -18,6 +18,7 @@ resource "azurerm_public_ip" "example1" {
   name                = "PublicIPForLB"
   location            = azurerm_resource_group.example1.location
   resource_group_name = azurerm_resource_group.example1.name
+}
   
 
 resource "azurerm_lb" "example1" {
