f67219ad846095f62bd6214e023cb934079a8aee
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-15 19:34:30 -0500

    Update readyaml_waf.tf

--- readyaml_waf.tf
+++ readyaml_waf.tf
@@ -1,9 +1,9 @@
 locals{
-  waf_policy=[for f in fileset("${path.module}/waffolder", "[^_]*.yaml") : yamldecode(file("${path.module}/waffolder/${f}"))]
+  waf_policy=[for f in fileset("${path.module}/waf_config", "[^_]*.yaml") : yamldecode(file("${path.module}/waf_config/${f}"))]
   azurewafpolicy_list = flatten([
     for app in local.waf_policy: [
       for azurewaf in try(app.listofwafpolicy, []) :{
-        name=azurewaf.policyname
+        name=azurewaf.name
       }
     ]
 ])
