3d00796ff917bbf1cb779ac57e7811d439d51b5f
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-15 19:22:52 -0500

    Update readyaml_waf.tf

--- readyaml_waf.tf
+++ readyaml_waf.tf
@@ -4,9 +4,7 @@ locals{
     for app in local.waf_app : [
       for wafapps in try(app.listofwafapp, []) :{
         name=wafapps.name
-        resource_group_name=azurerm_resource_group.mcit420zz5um.name
-        location=azurerm_resource_group.mcit420zz5um.location
-      
+              
       }
     ]
 ])
