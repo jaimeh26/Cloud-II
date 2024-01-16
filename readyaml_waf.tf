d4b7931579e85704192a4ec0e56422355cfc6fc5
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-15 19:18:06 -0500

    Update readyaml_waf.tf

--- readyaml_waf.tf
+++ readyaml_waf.tf
@@ -1,7 +1,7 @@
 locals{
-  wafapp=[for f in fileset("${path.module}/waf_configs, "[^_]*.yaml") : yamldecode(file("${path.module}/waf_config/${f}"))]
+  waf_app=[for f in fileset("${path.module}/waf_configs, "[^_]*.yaml") : yamldecode(file("${path.module}/waf_config/${f}"))]
   waf_app_list= flatten([
-    for app in local.wafapp : [
+    for app in local.waf_app : [
       for wafapps in try(app.listofwafapp, []) :{
         name=wafapps.name
         resource_group_name=azurerm_resource_group.mcit420zz5um.name
