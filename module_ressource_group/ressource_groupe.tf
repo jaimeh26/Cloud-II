0f0f97f1129d2b1e61ba7cc791caca9540f80f9e
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-17 19:48:27 -0500

    Update ressource_groupe.tf

--- module_ressource_group/ressource_groupe.tf
+++ module_ressource_group/ressource_groupe.tf
@@ -13,3 +13,15 @@ resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
 }
+
+output "resource_group_id" {
+  value = azurerm_resource_group.example.id
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.example.location
+}
