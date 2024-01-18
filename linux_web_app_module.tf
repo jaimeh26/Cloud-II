554072f656c80e29434834d3d86b7cb6f21ce132
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-17 20:53:52 -0500

    Update linux_web_app_module.tf

--- linux_web_app_module.tf
+++ linux_web_app_module.tf
@@ -5,3 +5,15 @@ module "example" {
   service_plan_name   = "example"
   web_app_name        = "example"
 }
+
+output "resource_group_id" {
+  value = azurerm_resource_group.example.id
+}
+
+output "service_plan_id" {
+  value = azurerm_service_plan.example.id
+}
+
+output "web_app_id" {
+  value = azurerm_linux_web_app.example.id
+}
