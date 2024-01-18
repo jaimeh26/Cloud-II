922e9c3f3ec714e707636bc637662b993baead39
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-17 20:09:24 -0500

    Update resource_group_module.tf

--- resource_group_module.tf
+++ resource_group_module.tf
@@ -1,1 +1,15 @@
+module "resource_group" {
+  source = "./module_resource_group/"
 
+  resource_group_name   = "myresourcegroup"
+  location              = "eastus"
+  
+}
+
+output "resource_group_name" {
+  value = module.resource_group.resource_group_name
+}
+
+output "resource_group_id" {
+  value = module.resource_group.resource_group_id
+}
