4ffe51f066700e38c773cbc2d823d9cc46c55791
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-17 19:39:10 -0500

    Update ressource_groupe.tf

--- module_ressource_group/ressource_groupe.tf
+++ module_ressource_group/ressource_groupe.tf
@@ -9,29 +9,3 @@ variable "location" {
   default     = "eastus"
 }
 
-variable "account_tier" {
-  description = "The storage account tier (Standard or Premium)"
-  default     = "Standard"
-}
-
-variable "account_replication_type" {
-  description = "The storage account replication type"
-  default     = "LRS"
-}
-
-resource "azurerm_storage_account" "storage_account" {
-  name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-
-}
-
-output "storage_account_name" {
-  value = azurerm_storage_account.storage_account.name
-}
-
-output "storage_account_id" {
-  value = azurerm_storage_account.storage_account.id
-}
