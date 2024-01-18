67c497e9ca2299156741f4504b3557c278e1cd5d
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-17 20:07:59 -0500

    Update resource_group.tf

--- module_resource_group/resource_group.tf
+++ module_resource_group/resource_group.tf
@@ -1,1 +1,27 @@
+#module for Resource Group
 
+variable "resource_group_name" {
+    description = "The name of the Azure Resource Group"
+}
+
+variable "location" {
+    description = "The Azure region where the Resource Group will be created"
+    default     = "eastus"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+output "resource_group_id" {
+  value = azurerm_resource_group.example.id
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.example.location
+}
