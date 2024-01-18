a9e2ec552013cbcbb087168829295fbfc842b6b8
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-17 19:53:22 -0500

    Update ressource_group_module.tf

--- ressource_group_module.tf
+++ ressource_group_module.tf
@@ -1,1 +1,15 @@
+module "ressource_group" {
+  source = "./module_ressource_group/"
 
+  resource_group_name   = "myresourcegroup"
+  location              = "eastus"
+  
+}
+
+output "storage_account_name" {
+  value = module.storage_account.storage_account_name
+}
+
+output "storage_account_id" {
+  value = module.storage_account.storage_account_id
+}
