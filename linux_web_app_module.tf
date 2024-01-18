ac89e9b55d2e959488ceb739437014c4b8d850b2
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-17 20:58:22 -0500

    Update linux_web_app_module.tf

--- linux_web_app_module.tf
+++ linux_web_app_module.tf
@@ -1,7 +1,7 @@
 module "example" {
   source               = "./module_linux_web_app"
   resource_group_name = "example-resources"
-  location            = var.location
+  location            = "eastus"
   service_plan_name   = "example"
   web_app_name        = "example"
 }
