deac07ce0b95b69ffdb7d842b9afc575610fd646
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-17 20:32:27 -0500

    Update linux_web_app.tf

--- module_linux_web_app/linux_web_app.tf
+++ module_linux_web_app/linux_web_app.tf
@@ -1,1 +1,18 @@
+variable "resource_group_name" {
+  description = "The name of the Azure Resource Group."
+  
+}
 
+variable "location" {
+  description = "The Azure region where resources will be deployed."
+  
+
+variable "service_plan_name" {
+  description = "The name of the Azure App Service Plan."
+  
+}
+
+variable "web_app_name" {
+  description = "The name of the Azure Linux Web App."
+}
+  
