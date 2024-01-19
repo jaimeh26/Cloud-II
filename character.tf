a7b99e8839db0a5db71d898071fce95b49ab4240
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-19 18:38:57 -0500

    Update character.tf

--- character.tf
+++ character.tf
@@ -2,9 +2,8 @@ locals{
   characters = ["luke", "yoda", "darth"]
   enemies_destroyed = [4252, 900, 20000056894]
   }
-}
 
 output "character_mapping"{
-value={for index,character in local.characters:
+  value={for index,character in local.characters:
       character => local.enemies_destroyed[index]
 }
