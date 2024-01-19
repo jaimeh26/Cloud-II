f2980f78d7d1a0532e2f0101097b80a29c1ac75a
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-19 18:21:58 -0500

    Create nested_map.tf

---
+++ nested_map.tf
@@ -0,0 +1,13 @@
+variable "nested_map" {
+  type    = map(map(string))
+  default = {
+    group1 = {"name" = "Bob", "age" = "25"}
+    group2 = {"name" = "Alice", "age" = "30"}
+  }
+}
+locals{
+ flattened_map  = flatten([for k, v in var.nested_map : [for kk, vv in v : { group = k, key = kk, value = vv }]])
+   }
+output "flattened_map_result" {
+  value = local.flattened_map
+}
