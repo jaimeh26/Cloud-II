f27edbad038825c4fd58d4d2874429624c731838
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-19 19:57:34 -0500

    Update readyaml_windows_app.tf

--- readyaml_windows_app.tf
+++ readyaml_windows_app.tf
@@ -6,10 +6,12 @@ locals{
   windowsconfig=[for f in fileset("${path.module}/windows_app_config", "[^_]*.yaml") : yamldecode(file("${path.module}/windows_app_config/${f}"))]
   windows_apps = flatten([
     for app in local.windowsconfig : [
-      for windowsapps in try(app.windowsfunctionapp, []) :{
-        name=linuxapps.name
-        os_type=linuxapps.os_type
-        sku_name=linuxapps.sku_name     
+      for windowsapp in try(app.windowsfunctionapp, []) :{
+        name: windowsapp.name
+        account_tier: windowsapp.account_tier
+        account_replication_type: windowsapp.account_replication_type
+        os_type: windowsapp.os_type
+        sku_name: windowsapp.sku_name    
       }
     ]
 ])
