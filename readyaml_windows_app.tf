2debb4f05951ddd95def97c1008acdc692e4bad8
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-19 19:42:01 -0500

    Update readyaml_windows_app.tf

--- readyaml_windows_app.tf
+++ readyaml_windows_app.tf
@@ -2,6 +2,19 @@ provider "azurerm" {
   features {}
 }
 
+locals{
+  linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
+  linux_app_list = flatten([
+    for app in local.linux_app : [
+      for linuxapps in try(app.listoflinuxapp, []) :{
+        name=linuxapps.name
+        os_type=linuxapps.os_type
+        sku_name=linuxapps.sku_name     
+      }
+    ]
+])
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
