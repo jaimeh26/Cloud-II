748be7d6174be3383c118ff989e9f0342d766b58
jaimeh26 <145076627+jaimeh26@users.noreply.github.com>
2024-01-19 20:00:25 -0500

    Update readyaml_windows_app.tf

--- readyaml_windows_app.tf
+++ readyaml_windows_app.tf
@@ -21,3 +21,10 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "windowsfunctionappsa"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
